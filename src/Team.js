e76524ba1e846b10209be72001fceeb1fe581db2
Nemanja Milicevic <the.nemanja.milicevic@gmail.com>
2021-12-13 19:47:43 +0100

    Add team social media links

--- src/Team.js
+++ src/Team.js
@@ -3,6 +3,7 @@ import mimiImage from "./images/mimi.svg";
 import dakaImage from "./images/daka.svg";
 import anyaImage from "./images/anya.svg";
 import greesinImage from "./images/greesin.svg";
+import "./Team.scss";
 
 function Team() {
   return (
@@ -15,26 +16,82 @@ function Team() {
           <div className="col text-center">
             <img src={djapeImage} alt="Oops.." width="200" height="200" />
             <h2 className="">DJ Ape</h2>
-            <p className="lead">True friend. Squad goals.</p>
-          </div>
-          <div className="col text-center">
-            <img src={mimiImage} alt="Oops.." width="200" height="200" />
-            <h2 className="">Mimi</h2>
             <p className="lead">
-              Adventurer. Experience over things. Journey over destination.
+              Engineer who found a combination of programming, mathematics and
+              research in artificial intelligence. I find fun in games, anime,
+              books and interesting discussions. Always seeking new knowledge,
+              ways to improve myself and also challenging problems that will
+              push me out of my comfort zone. Web 3.0 is next on my list.
             </p>
+            <a
+              href="https://www.linkedin.com/in/predrag-njegovanovic/"
+              target="_blank"
+              rel="noreferrer"
+              className="TeamIcon"
+            >
+              <i className="bi bi-linkedin Icon-linkedin" />
+            </a>
           </div>
           <div className="col text-center">
             <img src={dakaImage} alt="Oops.." width="200" height="200" />
             <h2 className="">Daka</h2>
             <p className="lead">
-              Chad. It was an easy decision who Chad is in this group!
+              The creative force behind MeVerse. 2D animator by day, cartoon
+              artist by night. Thinks that visuals express ideas, vision,
+              beliefs, culture, and purpose faster, better, and more deeply than
+              any other method of communication.
             </p>
+            <a
+              href="https://www.linkedin.com/in/david-dubajic-8b9920197/"
+              target="_blank"
+              rel="noreferrer"
+              className="TeamIcon"
+            >
+              <i className="bi bi-linkedin Icon-linkedin" />
+            </a>
+          </div>
+          <div className="col text-center">
+            <img src={mimiImage} alt="Oops.." width="200" height="200" />
+            <h2 className="">Mimi</h2>
+            <p className="lead">
+              Machine Learning Engineer with strong Power Engineering
+              fundamentals. Looking for new things, hungry for experience.
+              Believes that Virtual Worlds are a new frontier.
+            </p>
+            <a
+              href="https://www.linkedin.com/in/milivoje-popovac-93344217b/"
+              target="_blank"
+              rel="noreferrer"
+              className="TeamIcon"
+            >
+              <i className="bi bi-linkedin Icon-linkedin" />
+            </a>
           </div>
           <div className="col text-center">
             <img src={anyaImage} alt="Oops.." width="200" height="200" />
             <h2 className="">Anya</h2>
-            <p className="lead">Thinker. Building Web 3.0, block by block.</p>
+            <p className="lead">
+              Thrilled about building intelligent systems. Always looking for
+              new ways to improve things. Currently focused on building Web 3.0,
+              block by block.
+            </p>
+            <a
+              href="https://www.linkedin.com/in/nemanja-milicevic"
+              target="_blank"
+              rel="noreferrer"
+              className="TeamIcon"
+            >
+              <i className="bi bi-linkedin Icon-linkedin" />
+            </a>
+            &nbsp;&nbsp;
+            <a
+              href="https://twitter.com/itz_nemanja"
+              target="_blank"
+              rel="noreferrer"
+              className="TeamIcon"
+            >
+              <i className="bi bi-twitter Icon-twitter" />
+            </a>
           </div>
           <div className="col text-center">
             <img src={greesinImage} alt="Oops.." width="200" height="200" />
@@ -43,6 +100,14 @@ function Team() {
               Leader. Marching with my team on the frontiers of the Metaverse,
               driven by innovation.
             </p>
+            <a
+              href="https://twitter.com/FilipBaturan"
+              target="_blank"
+              rel="noreferrer"
+              className="TeamIcon"
+            >
+              <i className="bi bi-twitter Icon-twitter" />
+            </a>
           </div>
         </div>
       </div>
